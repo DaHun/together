127628694f7bb55c5a4a2f08639d14a39df9b1b9
gonghojin <rhdghwls5@naver.com>
2017-11-03 18:24:10 +0900

    개인정보 입력 여부 에 따른 로그인 처리 완료

--- Together/app/src/main/java/test/project/together/login/LoginActivity.java
+++ Together/app/src/main/java/test/project/together/login/LoginActivity.java
@@ -1,5 +1,7 @@
 package test.project.together.login;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
@@ -14,6 +16,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -25,6 +28,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import test.project.together.R;
 import test.project.together.adapter.ViewPagerAdapter;
+import test.project.together.main.MainActivity;
 import test.project.together.model.ChangeEvent;
 import test.project.together.model.InfoLayoutEvent;
 import test.project.together.model.Matching;
@@ -33,13 +37,12 @@ import test.project.together.tab.VolunteerFragment;
 
 import static test.project.together.adapter.ViewPagerAdapter.subMode;
 
-public class LoginActivity extends AppCompatActivity implements android.widget.RadioGroup.OnCheckedChangeListener{
+public class LoginActivity extends AppCompatActivity{
 
     EditText nametxt;
     Spinner agespinner;
-    EditText phonetxt;
 
-    RadioGroup radio;
+    RadioGroup rg;
 
     Button signinbtn;
     Button cancelbtn;
@@ -63,8 +66,8 @@ public class LoginActivity extends AppCompatActivity implements android.widget.R
         cancelbtn=(Button)findViewById(R.id.sign_in_cancel);
         nametxt=(EditText)findViewById(R.id.sign_in_name);
         agespinner=(Spinner)findViewById(R.id.sign_in_age);
-        radio = (RadioGroup)findViewById(R.id.radio);
-        radio.setOnCheckedChangeListener(this);
+        rg = (RadioGroup)findViewById(R.id.radiogroup);
+
     }
 
 
@@ -94,15 +97,31 @@ public class LoginActivity extends AppCompatActivity implements android.widget.R
         //성별
 
 
+        //등록
         signinbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //이름
                 name = nametxt.getText().toString();
-                //전화번호
-                phone = phonetxt.getText().toString();
                 //age;
-                //gender;
+                //gender
+                int id = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton)findViewById(id);
+
+                //sharedpreferences
+                SharedPreferences pref = getSharedPreferences("Info",MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("name",name);
+                editor.putString("age",Integer.toString(age));
+                editor.putString("gender",rb.getText().toString());
+                editor.commit();
+
+
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
                 //서버에 저장
             }
         });
@@ -111,17 +130,6 @@ public class LoginActivity extends AppCompatActivity implements android.widget.R
 
 
     }
-
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, @IdRes int i) {
-        if(radio.getCheckedRadioButtonId() == R.id.radio_boy){
-            gender = 0;//성별 남자
-        }else{
-            gender = 1;//성별 여자
-        }
-
-    }
-
 
 }
 
