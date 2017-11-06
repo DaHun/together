5517f682d7cc31bb352a29377a45fba84d608b1d
Eunju-Kim <kuj_1218@naver.com>
2017-11-06 16:11:44 +0900

    위치권한 물어봄, 로그기록 남기는 것 동의 구함

--- Together/app/src/main/java/test/project/together/tab/SeniorFragment.java
+++ Together/app/src/main/java/test/project/together/tab/SeniorFragment.java
@@ -1,13 +1,20 @@
 package test.project.together.tab;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import org.greenrobot.eventbus.EventBus;
 
@@ -16,7 +23,6 @@ import butterknife.ButterKnife;
 import test.project.together.R;
 import test.project.together.adapter.ViewPagerAdapter;
 import test.project.together.model.ChangeEvent;
-import test.project.together.model.Posting;
 
 /**
  * Created by jeongdahun on 2017. 9. 11..
@@ -69,8 +75,46 @@ public class SeniorFragment extends Fragment {
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ViewPagerAdapter.subMode=1;
-                EventBus.getDefault().post(new ChangeEvent());
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    int permissionResult = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION);
+                    /**
+                     * 패키지는 안드로이드 어플리케이션의 아이디이다.
+                     * 현재 어플리케이션이 ACCESS_FINE_LOCATION에 대해 거부되어있는지 확인한다.
+                     * */
+                    if (permissionResult == PackageManager.PERMISSION_DENIED) {
+                        /** * 사용자가 ACCESS_FINE_LOCATION 권한을 거부한 적이 있는지 확인한다.
+                         * 거부한적이 있으면 True를 리턴하고
+                         * 거부한적이 없으면 False를 리턴한다. */
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                            AlertDialog.Builder dialog2 = new AlertDialog.Builder(getActivity().getApplication());
+                            dialog2.setTitle("Permission is required.") .setMessage("\"Location\" permission is required to use this function. Do you want to continue?") .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    /**
+                                     * 새로운 인스턴스(onClickListener)를 생성했기 때문에
+                                     * 버전체크를 다시 해준다.
+                                     * */
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // ACCESS_FINE_LOCATION 권한을 Android OS에 요청한다.
+                                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000); } } }) .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getActivity().getApplication(), "Canceled.", Toast.LENGTH_SHORT).show(); } }) .create() .show();
+                        } // 최초로 권한을 요청할 때
+                        else { // ACCESS_FINE_LOCATION 권한을 Android OS에 요청한다.
+                            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+                        }
+                    } // ACCESS_FINE_LOCATION 권한이 있을 때
+                    else { // 즉시 실행
+                        //RegisterFragment로 이동
+                        ViewPagerAdapter.subMode=1;
+                        EventBus.getDefault().post(new ChangeEvent());
+                    }
+                } // 마시멜로우 미만의 버전일 때
+                else { // 즉시 실행
+                    //RegisterFragment로 이동
+                    ViewPagerAdapter.subMode=1;
+                    EventBus.getDefault().post(new ChangeEvent());
+                }
             }
         });
 
@@ -81,10 +125,5 @@ public class SeniorFragment extends Fragment {
                 EventBus.getDefault().post(new ChangeEvent());
             }
         });
-
-
     }
-
-
-
 }
