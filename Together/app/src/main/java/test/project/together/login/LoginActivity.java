11fddefc7e566f21d9c6e808e7b8c0eec809527b
Eunju-Kim <kuj_1218@naver.com>
2017-11-12 20:55:21 +0900

    로그인 할 때 token 포함한 User 객체 만들어서 넘겼음

--- Together/app/src/main/java/test/project/together/login/LoginActivity.java
+++ Together/app/src/main/java/test/project/together/login/LoginActivity.java
@@ -1,13 +1,14 @@
 package test.project.together.login;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,28 +17,26 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import java.util.ArrayList;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import test.project.together.R;
-import test.project.together.adapter.ViewPagerAdapter;
+import test.project.together.application.ApplicationController;
+import test.project.together.gcm.QuickstartPreferences;
+import test.project.together.gcm.RegistrationIntentService;
 import test.project.together.main.MainActivity;
-import test.project.together.model.ChangeEvent;
-import test.project.together.model.InfoLayoutEvent;
-import test.project.together.model.Matching;
-import test.project.together.model.Posting;
-import test.project.together.tab.VolunteerFragment;
-
-import static test.project.together.adapter.ViewPagerAdapter.subMode;
+import test.project.together.model.User;
+import test.project.together.network.NetworkService;
 
 public class LoginActivity extends AppCompatActivity{
+
+    private static final String TAG = "LoginActivity";
 
     EditText nametxt;
     Spinner agespinner;
@@ -49,19 +48,32 @@ public class LoginActivity extends AppCompatActivity{
 
     String name;
     String phone;
+    String token;
     int age;
     int gender;
+
+    NetworkService service;
+
+    ///
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
 
+        getInstanceIdToken();
+        registBroadcastReceiver();
+
         initSetting();
         init();
     }
 
     public void initSetting(){
+
+        service= ApplicationController.getInstance().getNetworkService();
+
         signinbtn=(Button)findViewById(R.id.sign_in_btn);
         cancelbtn=(Button)findViewById(R.id.sign_in_cancel);
         nametxt=(EditText)findViewById(R.id.sign_in_name);
@@ -116,6 +128,25 @@ public class LoginActivity extends AppCompatActivity{
                 editor.putString("gender",rb.getText().toString());
                 editor.commit();
 
+                User user = new User(name, phone, Integer.toString(age), Integer.toString(gender), token);
+
+                Call<Void> registerUserInfo = service.registerUserInfo(user);
+                registerUserInfo.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        if(response.isSuccessful())
+                            Log.d(TAG,"success");
+                        else
+                            Log.d(TAG,"fail1");
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Log.d(TAG,"fail2");
+
+                    }
+                });
 
                 Intent intent=new Intent(getApplicationContext(),MainActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -129,6 +160,89 @@ public class LoginActivity extends AppCompatActivity{
 
 
 
+    }
+
+    ///////////
+
+
+    public void getInstanceIdToken() {
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
+    }
+
+    /**
+     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
+     */
+    public void registBroadcastReceiver(){
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+
+                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
+                    // 액션이 READY일 경우
+
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
+                    // 액션이 GENERATING일 경우
+
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
+                    // 액션이 COMPLETE일 경우
+                    //Toast.makeText(getApplicationContext(),"COMPLETE",Toast.LENGTH_SHORT).show();
+
+                    token=intent.getStringExtra("token");
+                    //Log.d(TAG,token);
+                    ApplicationController.token=token;
+
+                }
+            }
+        };
+    }
+
+    /**
+     * 앱이 실행되어 화면에 나타날때 LocalBoardcastManager에 액션을 정의하여 등록한다.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+
+    }
+
+    /**
+     * 앱이 화면에서 사라지면 등록된 LocalBoardcast를 모두 삭제한다.
+     */
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+
+    /**
+     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 
 }
