f77113f3272f2bffd413df0a2fe195bf7bffb1fd
Eunju-Kim <kuj_1218@naver.com>
2017-11-27 16:38:10 +0900

    다시추가

--- Together/app/src/main/java/test/project/together/adapter/CommentRecyclerViewAdapter.java
+++ Together/app/src/main/java/test/project/together/adapter/CommentRecyclerViewAdapter.java
@@ -1,6 +1,9 @@
 package test.project.together.adapter;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
 import test.project.together.R;
 import test.project.together.model.Comment;
@@ -22,6 +27,8 @@ public class CommentRecyclerViewAdapter extends RecyclerView.Adapter<CommentView
     ArrayList<Comment> items;
     Context context;
 
+    public TextToSpeech tts;
+
     public CommentRecyclerViewAdapter(ArrayList<Comment> items){
         this.items=items;
     }
@@ -35,12 +42,39 @@ public class CommentRecyclerViewAdapter extends RecyclerView.Adapter<CommentView
     }
 
     @Override
-    public void onBindViewHolder(CommentViewHolder holder, final int position) {
+    public void onBindViewHolder(final CommentViewHolder holder, final int position) {
         final Comment item=items.get(position);
 
         holder.userName.setText(item.getUser_name());
-        holder.date.setText(item.getDate());
+        String date = item.getDate().toString().substring(0,10);
+        String time = item.getDate().toString().substring(11,16);
+        holder.date.setText(date + " " +time);
         holder.content.setText(item.getContent());
+
+        tts=new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.ENGLISH);
+                }
+            }
+
+        });
+
+        holder.readbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = holder.content.getText().toString();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ttsGreater21(txt);
+                } else {
+                    ttsUnder20(txt);
+                }
+            }
+        });
+        //여기다가 구현
+
 
     }
 
@@ -50,5 +84,20 @@ public class CommentRecyclerViewAdapter extends RecyclerView.Adapter<CommentView
             return items.size();
         else
             return 0;
+    }
+
+
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId=this.hashCode() + "";
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
     }
 }
--- Together/app/src/main/java/test/project/together/viewholder/CommentViewHolder.java
+++ Together/app/src/main/java/test/project/together/viewholder/CommentViewHolder.java
@@ -2,6 +2,7 @@ package test.project.together.viewholder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import test.project.together.R;
@@ -10,12 +11,13 @@ public class CommentViewHolder extends RecyclerView.ViewHolder {
     public TextView userName;
     public TextView content;
     public TextView date;
-
+    public Button readbtn;
 
     public CommentViewHolder(View itemView) {
         super(itemView);
         userName=(TextView) itemView.findViewById(R.id.userName);
         content = (TextView)itemView.findViewById(R.id.content);
         date = (TextView)itemView.findViewById(R.id.date);
+        readbtn = (Button)itemView.findViewById(R.id.sound);
     }
 }
